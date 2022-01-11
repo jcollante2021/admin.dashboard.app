351915b18fd85304350c92dccda8c608e30d960b
jcollante2021 <jcollante@outlook.com.ar>
2022-01-11 12:02:51 -0300

    Creando SideBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import Sidebar from "./components/sidebar/Sidebar.jsx";
 import Topbar from "./components/topbar/Topbar.jsx";
-
+import "./app.css"
 
 function App() {
   return (
     <div>
       <Topbar/>
+      <div className="container">
+        <Sidebar/>
+        <div className="others">
+          otras paginas
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,83 @@
+import "./sidebar.css"
+import { LineStyle, VerifiedUser, ErrorOutline, Forum, InsertComment, MailOutline, Timeline, TrendingUp, Person, Storefront, AttachMoney, BarChart } from "@material-ui/icons"
+
+export default function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebarWrapper">
+                <div className="sidebarMenu">
+                    <h3 className="sidebarTitle">Dashboard</h3>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem">
+                            <LineStyle className="sidebarIcon"/>
+                            Home
+                        </li>
+                        <li className="sidebarListItem">
+                            <Timeline className="sidebarIcon"/>
+                            Analytics
+                        </li>
+                        <li className="sidebarListItem">
+                            <TrendingUp className="sidebarIcon"/>
+                            Sales
+                        </li>
+                    </ul>
+                </div>
+                <div className="sidebarMenu">
+                    <h3 className="sidebarTitle">Quick Menu</h3>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem">
+                            <Person className="sidebarIcon"/>
+                            Users
+                        </li>
+                        <li className="sidebarListItem">
+                            <Storefront className="sidebarIcon"/>
+                            Products
+                        </li>
+                        <li className="sidebarListItem">
+                            <AttachMoney className="sidebarIcon"/>
+                            Transactions
+                        </li>
+                        <li className="sidebarListItem">
+                            <BarChart className="sidebarIcon"/>
+                            Reports
+                        </li>
+                    </ul>
+                </div>
+                <div className="sidebarMenu">
+                    <h3 className="sidebarTitle">Notificactions</h3>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem">
+                            <MailOutline className="sidebarIcon"/>
+                            Mail
+                        </li>
+                        <li className="sidebarListItem">
+                            <InsertComment className="sidebarIcon"/>
+                            Feedback
+                        </li>
+                        <li className="sidebarListItem">
+                            <Forum className="sidebarIcon"/>
+                            Messages
+                        </li>
+                    </ul>
+                </div>
+                <div className="sidebarMenu">
+                    <h3 className="sidebarTitle">Staff</h3>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem">
+                            <VerifiedUser className="sidebarIcon"/>
+                            Manage
+                        </li>
+                        <li className="sidebarListItem">
+                            <Timeline className="sidebarIcon"/>
+                            Analytics
+                        </li>
+                        <li className="sidebarListItem">
+                            <ErrorOutline className="sidebarIcon"/>
+                            Reports
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
