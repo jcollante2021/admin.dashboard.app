a508d2fe725bad2b1fe9315090ab89929d53a1d4
jcollante2021 <jcollante@outlook.com.ar>
2022-01-11 15:20:13 -0300

    Correcciones

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -7,7 +7,7 @@ export default function Topbar() {
         <div className='topbar'>
             <div className='topbarWrapper'>
                 <div className="topLeft">
-                    <span className="logo">Admin DashBoard</span>
+                    <span className="logo">ADMIN DashBoard</span>
                 </div>
                 <div className="topRight">
                     <div className="topbarIconContainer">
